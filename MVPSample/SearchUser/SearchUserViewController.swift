369daa20b2a7d1e9d5789884e46d4afda084a3a7
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-08-17 14:28:54 +0900

    デフォルトのViewControllerを削除し、SearchUserViewControllerをEntryPointに設定

---
+++ MVPSample/SearchUser/SearchUserViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchUserViewController.swift
+//  MVPSample
+//
+//  Created by SRA01 on 2020/08/17.
+//  Copyright © 2020 nishitaku. All rights reserved.
+//
+
+import UIKit
+
+class SearchUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
