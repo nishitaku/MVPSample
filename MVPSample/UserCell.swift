1cb17de3225f2721e4a3a847c6b2114ad33ade0b
nishitaku <29690066+nishitaku@users.noreply.github.com>
2020-08-17 16:05:00 +0900

    UserCell.xibの追加

---
+++ MVPSample/UserCell.swift
@@ -0,0 +1,26 @@
+//
+//  UserCell.swift
+//  MVPSample
+//
+//  Created by SRA01 on 2020/08/17.
+//  Copyright © 2020 nishitaku. All rights reserved.
+//
+
+import UIKit
+
+class UserCell: UITableViewCell {
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
